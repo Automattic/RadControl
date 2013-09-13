a250555fa40047ff66fdbdf3ba9a5f1c5535213f
Derek Springer <derekspringer@gmail.com>
2013-09-13 11:29:13 -0700

    Restored full version of adsense tags.
    We're grabbing them from fallback now and they aren't going direct to
    the page.

--- php/adsense.php
+++ php/adsense.php
@@ -1,37 +1,42 @@
 <?php
-class AdControl_Adsense {
-    /**
-     * Generate synchronous adsense code
-     *
-     * @since 0.1
-     */
-    public static function get_synchronous_adsense( $pub, $tag, $width, $height ) {
-        return <<<HTML
-        <script type="text/javascript"><!--
-        google_ad_client = "ca-$pub";
-        google_ad_slot = "$tag";
-        google_ad_width = $width;
-        google_ad_height = $height;
-        //-->
-        </script>
-HTML;
-    }
 
-    /**
-     * Generate asynchronous adsense code
-     *
-     * @since 0.1
-     */
-    public static function get_asynchronous_adsense( $pub, $tag, $width, $height ) {
-        return <<<HTML
-        <ins class="adsbygoogle"
-            style="display:inline-block;width:{$width}px;height:{$height}px"
-            data-ad-client="ca-$pub"
-            data-ad-slot="$tag"></ins>
-        <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        jQuery('.adsbygoogle').hide();
-        </script>
+class AdControl_Adsense {
+	/**
+	 * Generate synchronous adsense code
+	 *
+	 * @since 0.1
+	 */
+	public static function get_synchronous_adsense( $pub, $tag, $width, $height, $url = '' ) {
+		if ( $url )
+			$url = 'google_page_url = "' . $url . '";';
+
+		return <<<HTML
+		<script type="text/javascript">
+		<!--
+		google_ad_client = "ca-$pub";
+		google_ad_slot = "$tag";
+		google_ad_width = $width;
+		google_ad_height = $height;
+		$url
+		//-->
+		</script>
+		<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
 HTML;
-    }
+	}
+
+	/**
+	 * Generate asynchronous adsense code
+	 *
+	 * @since 0.1
+	 */
+	public static function get_asynchronous_adsense( $pub, $tag, $width, $height ) {
+		return <<<HTML
+		<script async src="http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<ins class="adsbygoogle"
+			 style="display:inline-block;width:300px;height:250px"
+			 data-ad-client="ca-pub-2972973862093363"
+			 data-ad-slot="7520462734"></ins>
+		<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+HTML;
+	}
 }
