79fe13b5c4f41c4366a81bb9af3ca3a84316d32f
Egill R. Erlendsson <egill@automattic.com>
2013-12-08 13:58:00 -1000

    i18n support
    Worked on i18n support for more strings
    Moved $tabs init into __construct(), adding i18n support

--- php/admin.php
+++ php/admin.php
@@ -21,11 +21,6 @@ class AdControl_Admin {
 		'enable_advanced_settings',
 	);
 	private $active_tab = 'adcontrol_settings';
-	private $tabs = array(
-		'adcontrol_settings' => 'AdControl Settings',
-		'adcontrol_advanced_settings' => 'Advanced Settings',
-		'adcontrol_earnings' => 'Earnings',
-	);
 	private $options = array();
 	private $options_advanced = array();
 	private $basic_settings_key = 'adcontrol_settings';
@@ -33,11 +28,17 @@ class AdControl_Admin {
 	private $plugin_options_key = 'adcontrol';
 	private $status;
 	private $show_revenue = true;
+	private $tabs = array();
 
 	/**
 	 * @since 0.1
 	 */
 	function __construct() {
+		$this->tabs = array(
+			'adcontrol_settings'          => __( 'AdControl Settings', $this->plugin_options_key ),
+			'adcontrol_advanced_settings' => __( 'Advanced Settings', $this->plugin_options_key ),
+			'adcontrol_earnings'          => __( 'Earnings', $this->plugin_options_key ),
+		);
 		$this->blog_id = Jetpack::get_option( 'id', 0 );
 		$this->options = get_option( $this->basic_settings_key, array() );
 		$this->options_advanced = get_option( $this->advanced_settings_key, array() );
@@ -152,7 +153,7 @@ class AdControl_Admin {
 		if ( 'signed' == $this->get_option( 'tos' ) || 'signed' == $settings[ 'tos' ] )
 			$to_save[ 'tos' ] = 'signed';
 		else
-			add_settings_error( 'tos', 'tos', __( 'You must agree to the Terms of Service.' ) );
+			add_settings_error( 'tos', 'tos', __( 'You must agree to the Terms of Service.', $this->plugin_options_key ) );
 
 		if ( 'no' == $settings[ 'show_to_logged_in' ] ) {
 			$to_save[ 'show_to_logged_in' ] = 'no';
@@ -182,7 +183,7 @@ class AdControl_Admin {
 			if ( preg_match( '/^(pub-)?(\d+)$/', $settings['adsense_publisher_id'], $matches ) )
 				$to_save[ 'adsense_publisher_id' ] = 'pub-' . esc_attr( $matches[2] );
 			else
-				add_settings_error( 'adsense_publisher_id', 'adsense_publisher_id', __( 'Publisher ID must be of form "pub-123456789"', 'adcontrol' ) );
+				add_settings_error( 'adsense_publisher_id', 'adsense_publisher_id', __( 'Publisher ID must be of form "pub-123456789"', $this->plugin_options_key ) );
 		}
 
 		if ( ! empty( $settings['adsense_fallback'] ) ) {
@@ -191,7 +192,7 @@ class AdControl_Admin {
 			if ( ! empty( $settings['adsense_fallback_tag_id'] ) && is_numeric( $settings['adsense_fallback_tag_id'] ) )
 				$to_save[ 'adsense_fallback_tag_id' ] = esc_attr( $settings['adsense_fallback_tag_id'] );
 			else
-				add_settings_error( 'adsense_fallback_tag_id', 'adsense_fallback_tag_id', __( 'Tag ID must be of form "123456789"', 'adcontrol' ) );
+				add_settings_error( 'adsense_fallback_tag_id', 'adsense_fallback_tag_id', __( 'Tag ID must be of form "123456789"', $this->plugin_options_key ) );
 
 			$to_save[ 'adsense_fallback_tag_unit' ] = esc_attr( $settings['adsense_fallback_tag_unit'] );
 
@@ -205,7 +206,7 @@ class AdControl_Admin {
 			if ( ! empty( $settings['adsense_leader_tag_id'] ) && is_numeric( $settings['adsense_leader_tag_id'] ) )
 				$to_save[ 'adsense_leader_tag_id' ] = esc_attr( $settings['adsense_leader_tag_id'] );
 			else
-				add_settings_error( 'adsense_leader_tag_id', 'adsense_leader_tag_id', __( 'Tag ID must be of form "123456789"', 'adcontrol' ) );
+				add_settings_error( 'adsense_leader_tag_id', 'adsense_leader_tag_id', __( 'Tag ID must be of form "123456789"', $this->plugin_options_key ) );
 
 			$to_save[ 'adsense_leader_tag_unit' ] = esc_attr( $settings['adsense_leader_tag_unit'] );
 
@@ -225,7 +226,7 @@ class AdControl_Admin {
 	 */
 	function admin_menu() {
 		add_options_page(
-			__( 'AdControl Settings', 'adcontrol' ),
+			__( 'AdControl Settings', $this->plugin_options_key ),
 			'AdControl',
 			'manage_options',
 			$this->plugin_options_key,
@@ -279,7 +280,7 @@ class AdControl_Admin {
 
 		add_settings_field(
 			'adcontrol_userdash_show_to_logged_in_id',
-			__( 'Show ads to:', 'adcontrol' ),
+			__( 'Show ads to:', $this->plugin_options_key ),
 			array( $this, 'setting_show_to_logged_in' ),
 			$this->basic_settings_key,
 			$section_name,
@@ -289,7 +290,7 @@ class AdControl_Admin {
 		// TODO replace when backfill situation is fixed
 		add_settings_field(
 			'adcontrol_userdash_enable_advanced_settings',
-			__( 'Enable Advanced Settings:', 'adcontrol' ),
+			__( 'Enable Advanced Settings:', $this->plugin_options_key ),
 			array( $this, 'setting_enable_advanced_settings' ),
 			$this->basic_settings_key,
 			$section_name,
@@ -300,14 +301,14 @@ class AdControl_Admin {
 		$section_name = 'adcontrol_section_general_tos';
 		add_settings_section(
 			$section_name,
-			__( 'WordAds Terms of Service', 'adcontrol' ),
+			__( 'WordAds Terms of Service', $this->plugin_options_key ),
 			'__return_null',
 			$this->basic_settings_key
 		);
 
 		add_settings_field(
 			'adcontrol_userdash_tos_id',
-			sprintf( __( 'I have read and agree to the %sWordAds Terms of Service', 'adcontrol' ), '<br /><a href="http://wordpress.com/tos-wordads/" target="_blank">' ) . '</a>',
+			sprintf( __( 'I have read and agree to the %sWordAds Terms of Service%s', $this->plugin_options_key ), '<br /><a href="http://wordpress.com/tos-wordads/" target="_blank">', '</a>' ),
 			array( $this, 'setting_tos' ),
 			$this->basic_settings_key,
 			$section_name,
@@ -323,14 +324,14 @@ class AdControl_Admin {
 		// AdSense section
 		add_settings_section(
 			$section,
-			__( 'AdSense Options', 'adcontrol' ),
+			__( 'AdSense Options', $this->plugin_options_key ),
 			'__return_null',
 			$this->advanced_settings_key
 		);
 
 		add_settings_field(
 			'adcontrol_userdash_publisher_id',
-			__( 'Publisher ID:', 'adcontrol' ),
+			__( 'Publisher ID:', $this->plugin_options_key ),
 			array( $this, 'setting_publisher_id' ),
 			$this->advanced_settings_key,
 			$section,
@@ -339,7 +340,7 @@ class AdControl_Admin {
 
 		add_settings_field(
 			'adcontrol_userdash_adsense_fallback',
-			__( 'Include AdSense fallback?', 'adcontrol' ),
+			__( 'Include AdSense fallback?', $this->plugin_options_key ),
 			array( $this, 'setting_adsense_fallback' ),
 			$this->advanced_settings_key,
 			$section,
@@ -348,7 +349,7 @@ class AdControl_Admin {
 
 		add_settings_field(
 			'adcontrol_userdash_fallback_tag_id',
-			__( 'Tag ID:', 'adcontrol' ),
+			__( 'Tag ID:', $this->plugin_options_key ),
 			array( $this, 'setting_fallback_tag_id' ),
 			$this->advanced_settings_key,
 			$section,
@@ -357,7 +358,7 @@ class AdControl_Admin {
 
 		add_settings_field(
 			'adcontrol_userdash_fallback_tag_unit',
-			__( 'Tag Dimensions:', 'adcontrol' ),
+			__( 'Tag Dimensions:', $this->plugin_options_key ),
 			array( $this, 'setting_fallback_tag_unit' ),
 			$this->advanced_settings_key,
 			$section,
@@ -366,7 +367,7 @@ class AdControl_Admin {
 
 		add_settings_field(
 			'adcontrol_userdash_adsense_leader',
-			__( 'Include AdSense leader?', 'adcontrol' ),
+			__( 'Include AdSense leader?', $this->plugin_options_key ),
 			array( $this, 'setting_adsense_leader' ),
 			$this->advanced_settings_key,
 			$section,
@@ -375,7 +376,7 @@ class AdControl_Admin {
 
 		add_settings_field(
 			'adcontrol_userdash_leader_tag_id',
-			__( 'Tag ID:', 'adcontrol' ),
+			__( 'Tag ID:', $this->plugin_options_key ),
 			array( $this, 'setting_leader_tag_id' ),
 			$this->advanced_settings_key,
 			$section,
@@ -384,7 +385,7 @@ class AdControl_Admin {
 
 		add_settings_field(
 			'adcontrol_userdash_leader_tag_unit',
-			__( 'Tag Dimensions:', 'adcontrol' ),
+			__( 'Tag Dimensions:', $this->plugin_options_key ),
 			array( $this, 'setting_leader_tag_unit' ),
 			$this->advanced_settings_key,
 			$section,
@@ -395,14 +396,14 @@ class AdControl_Admin {
 		// Amazon section
 		add_settings_section(
 			$section,
-			__( 'Amazon Matchbuy', 'adcontrol' ),
+			__( 'Amazon Matchbuy', $this->plugin_options_key ),
 			'__return_null',
 			$this->advanced_settings_key
 		);
 
 		add_settings_field(
 			'adcontrol_userdash_amazon_match_buy',
-			__( 'Enable Amazon Matchbuy?', 'adcontrol' ),
+			__( 'Enable Amazon Matchbuy?', $this->plugin_options_key ),
 			array( $this, 'setting_amazon_match_buy' ),
 			$this->advanced_settings_key,
 			$section,
@@ -420,11 +421,11 @@ class AdControl_Admin {
 			$show_to_logged_in = 'yes';
 		?>
 		<p><input type="radio" name="<?php echo esc_attr( $this->basic_settings_key ); ?>[show_to_logged_in]" id="radio_show_to_logged_in" value="yes" <?php checked( $show_to_logged_in, 'yes' ); ?>/>
-		<label for="radio_show_to_logged_in"> <?php _e( 'Every visitor' ); ?></label></p>
+		<label for="radio_show_to_logged_in"> <?php _e( 'Every visitor', $this->plugin_options_key ); ?></label></p>
 		<p><input type="radio" name="<?php echo esc_attr( $this->basic_settings_key ); ?>[show_to_logged_in]" id="radio_hide_from_logged_in" value="no" <?php checked( $show_to_logged_in, 'no' ); ?>/>
-		<label for="radio_hide_from_logged_in"><?php _e( 'Every visitor, except logged-in users (fewer impressions)' ); ?></label></p>
+		<label for="radio_hide_from_logged_in"><?php _e( 'Every visitor, except logged-in users (fewer impressions)', $this->plugin_options_key ); ?></label></p>
 		<p><input type="radio" name="<?php echo esc_attr( $this->basic_settings_key ); ?>[show_to_logged_in]" id="radio_hide_from_everyone" value="pause" <?php checked( $show_to_logged_in, 'pause' ); ?>/>
-		<label for="radio_hide_from_everyone"><?php _e( 'Do not show any ads' ); ?></label></p>
+		<label for="radio_hide_from_everyone"><?php _e( 'Do not show any ads', $this->plugin_options_key ); ?></label></p>
 		<?php
 	}
 
@@ -459,7 +460,7 @@ class AdControl_Admin {
 	function setting_publisher_id() {
 		$pid = $this->get_option( 'adsense_publisher_id' );
 		echo "<input type='text' name='" . $this->advanced_settings_key . "[adsense_publisher_id]' value='$pid' /> ";
-		_e( 'e.g. pub-123456789', 'adsense' );
+		_e( 'e.g. pub-123456789', $this->plugin_options_key );
 		echo '<div class="aligncenter" style="width:290px;"><hr /></div>';
 	}
 
@@ -478,7 +479,7 @@ class AdControl_Admin {
 		$tid = $this->get_option( 'adsense_fallback_tag_id' );
 		$disabled = disabled( ! $this->get_option( 'adsense_fallback' ), true, false );
 		echo "<input class='adsense_fallback_opt' ", esc_attr( $disabled ) ,"type='text' name='" , esc_attr( $this->advanced_settings_key ) , "[adsense_fallback_tag_id]' value='" , esc_attr( $tid ), "' /> ";
-		_e( 'e.g. 123456789', 'adsense' );
+		_e( 'e.g. 123456789', $this->plugin_options_key );
 	}
 
 	/**
@@ -488,7 +489,7 @@ class AdControl_Admin {
 		$tid = $this->get_option( 'adsense_leader_tag_id' );
 		$disabled = disabled( ! $this->get_option( 'adsense_leader' ), true, false );
 		echo "<input class='adsense_leader_opt' ", esc_attr( $disabled ), " type='text' name='" , esc_attr( $this->advanced_settings_key ) , "[adsense_leader_tag_id]' value='" , esc_attr( $tid ), "' /> ";
-		_e( 'e.g. 123456789', 'adsense' );
+		_e( 'e.g. 123456789', $this->plugin_options_key );
 	}
 
 	/**
@@ -537,7 +538,7 @@ class AdControl_Admin {
 		if ( 'signed' != $this->get_option( 'tos' ) )
 			echo '<p><input type="checkbox" name="' . $this->basic_settings_key . '[tos]" id="chk_agreement" value="signed" /></p>';
 		else
-			echo '<span class="checkmark"></span>' .  __( 'Thank you for accepting the WordAds Terms of Service', 'adcontrol' );
+			echo '<span class="checkmark"></span>' .  __( 'Thank you for accepting the WordAds Terms of Service', $this->plugin_options_key );
 	}
 
 	/**
