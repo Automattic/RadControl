7405d01b4a3ea29ab121b48377d0f24696f4bb66
Itamar Eduardo Gonçalves de Oliveira <3duardo.goncalves@gmail.com>
2018-10-11 20:15:32 -0400

    moved amp_post_template_css call to insert_adcode()

--- adcontrol.php
+++ adcontrol.php
@@ -149,6 +149,7 @@ class AdControl {
 		add_action( 'wp_head', array( $this, 'insert_head_meta' ), 20 );
 		add_action( 'wp_head', array( $this, 'insert_head_iponweb' ), 30 );
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_action( 'amp_post_template_css', array( $this, 'insert_head_amp_ad_styles' ) );
 
 		if ( ! apply_filters( 'adcontrol_content_disable', false ) ) {
 			add_filter( 'the_content', array( $this, 'insert_ad' ) );
@@ -233,6 +234,25 @@ HTML;
 		<script$data_tags async type="text/javascript" src="//s.pubmine.com/head.js"></script>
 HTML;
 	}
+
+	/**
+	 * Add Wordads Style in <head> of AMP pages
+	 */
+	function insert_head_amp_ad_styles() {
+
+		if ( ! ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) ) {
+			return;
+		}
+		
+		echo <<<HTML
+		.wpa {
+			position: relative;
+			overflow: hidden;
+			display: inline-block;
+			max-width: 100%;
+		}
+HTML;
+  }
 
 	/**
 	 * Insert the ad onto the page
@@ -391,22 +411,21 @@ HTML;
 		$data_tags = $this->params->cloudflare ? ' data-cfasync="false"' : '';
 		$adblock_ad = $this->get_adblocker_ad( $adblock_unit );
 
-		if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
-
-      add_action( 'amp_post_template_css', array( $this, 'add_ad_styles' ) );
+		if ( current_theme_supports('amp') ||
+				( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) ) {
 
 			$blog_id =  substr( $section_id, 0, -1 );
 			$section = substr( $section_id, -1 );
 			return <<<HTML
-      <div style="padding-bottom:15px;width:{$width}px;height:{$height}px;$css">
-        <amp-ad width="{$width}" height="{$height}"
-          type="pubmine"
-          data-section="{$section}"
-          data-pt="1"
-          data-ht="2"
-          data-siteid="{$blog_id}">
-        </amp-ad>
-      </div>
+			<div style="padding-bottom:15px;width:{$width}px;height:{$height}px;$css">
+				<amp-ad width="{$width}" height="{$height}"
+					type="pubmine"
+					data-section="{$section}"
+					data-pt="1"
+					data-ht="2"
+					data-siteid="{$blog_id}">
+				</amp-ad>
+			</div>
 HTML;
 		}
 
@@ -428,35 +447,7 @@ HTML;
 		</div>
 HTML;
 	}
-	/**
-	 * Add AMP Customizer preview styles.
-	 */
-	 public function add_ad_styles() {
-	?>
-	.wpa {
-		position: relative;
-		overflow: hidden;
-		display: inline-block;
-		max-width: 100%;
-	}
 
-	.wpa-about {
-		position: absolute;
-		top: 5px;
-		left: 0;
-		right: 0;
-		display: block;
-		margin-top: 0;
-		color: #888;
-		font: 10px/1 "Open Sans",Arial,sans-serif!important;
-		text-align: left!important;
-		text-decoration: none!important;
-		opacity: .85;
-		border-bottom: none!important;
-		box-shadow: none!important;
-	}
-	<?php
-	}
 	/**
 	 * Get Criteo Acceptable Ad unit
 	 * @param  string $unit mrec, mrec2, widesky, top, top_mrec
